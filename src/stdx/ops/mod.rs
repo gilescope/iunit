3c129c5627cbb7f3bf6d459a0066b4035a250cb6
Giles Cope <gilescope@gmail.com>
2018-04-03 17:56:35 +0100

    Tidying of num tests.

--- src/stdx/ops/mod.rs
+++ src/stdx/ops/mod.rs
@@ -1,45 +1,45 @@
-use std::ops::{Add, Sub, Mul, Div};
 use std::fmt::Debug;
-use num::{Zero, One};
+use num::Num;
 
 #[trait_tests]
-pub trait AddSubTests : Add<Output=Self> + Sub<Output=Self> + Sized + Zero + One + Debug + Eq + Clone
+pub trait NumTests : Num + Debug
 {
     fn test_add_sub_zero() {
-        let original = Self::zero();
-
-        let added = original.clone() + Self::zero();
-
-        let result = added - Self::zero();
-
-        assert_eq!(result, original);
+        assert_eq!((Self::zero() + Self::zero()) - Self::zero(), Self::zero());
+        assert_eq!(Self::zero() + (Self::zero() - Self::zero()), Self::zero());
     }
 
     fn test_add_sub_one() {
-        let original = Self::zero();
+        assert!(Self::is_one(&(Self::zero() + Self::one())));
 
-        let added = original.clone() + Self::one();
-
-        assert!(Self::is_one(&added));
-
-        let result = added - Self::one();
-
-        assert_eq!(result, original);
+        assert_eq!((Self::zero() + Self::one()) - Self::one(), Self::zero());
     }
-}
 
-#[trait_tests]
-pub trait MulDivTests : Mul<Output=Self> + Div<Output=Self> + Sized + Zero + One + Debug + Eq + Clone
-{
     fn test_mul() {
-        let original = Self::zero();
-
-        let multiplied = original.clone() * Self::zero();
+        let multiplied = Self::zero() * Self::zero();
         assert!(Self::is_zero(&multiplied));
-        assert_eq!(multiplied, original);
+        assert_eq!(multiplied, Self::zero());
+    }
+
+    fn test_mul_identity() {
+        assert_eq!(Self::zero() * Self::one(), Self::zero());
+        assert_eq!(Self::one() * Self::one(), Self::one());
+    }
+
+    fn test_div_identity() {
+        assert_eq!(Self::zero() / Self::one(), Self::zero());
+        assert_eq!(Self::one() / Self::one(), Self::one());
     }
 }
 
-#[trait_tests] impl MulDivTests for usize { }
+#[trait_tests] impl NumTests for usize { }
+#[trait_tests] impl NumTests for u8 { }
+#[trait_tests] impl NumTests for u16 { }
+#[trait_tests] impl NumTests for u32 { }
+#[trait_tests] impl NumTests for u64 { }
 
-#[trait_tests] impl AddSubTests for usize { }
+#[trait_tests] impl NumTests for isize { }
+#[trait_tests] impl NumTests for i8 { }
+#[trait_tests] impl NumTests for i16 { }
+#[trait_tests] impl NumTests for i32 { }
+#[trait_tests] impl NumTests for i64 { }
